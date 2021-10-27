9a4903b90efb165798dac8963d4fd5c2d595185b
hongzj <hongzj@rimepevc.com>
2021-10-27 19:01:41 +0800

    test sh

---
+++ bilibli_run.sh
@@ -0,0 +1,7 @@
+'''
+
+[task_local]
+0 6 10 * * * https://github.com/zijiahong/bilibili/blob/bdf4ad93865e27de70bd9eee908ab960b92da73f/bilibili-helper.log
+
+'''
+java -jar /ql/repo/zijiahong_bilibili/BILIBILI-HELPER.jar /ql/repo/zijiahong_bilibili/config.json
