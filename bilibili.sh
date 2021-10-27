2704f763d4c862243880277bd5f067d6f9ce1aa8
hongzj <hongzj@rimepevc.com>
2021-10-27 18:21:27 +0800

    test

--- bilibili.sh
+++ bilibili.sh
@@ -1,4 +1,4 @@
 :<<!
 [Script]
-cron "10 0,7,23 * * *" script-path=jd_syj.js, tag=bilibili脚本测试
+cron "10 0 * * *" script-path=https://github.com/zijiahong/bilibili/blob/a31268371fea7024ecf6a22bec4f829550d23309/bilibili.sh,tag=bilibilitest
 !
