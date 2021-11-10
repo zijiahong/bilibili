2e4778743b0670ebcc82f8bb897cc7659e3c99eb
hongzijia <43508513+zijiahong@users.noreply.github.com>
2021-11-10 10:51:03 +0800

    Update bilibili_run.py

--- bilibili_run.py
+++ bilibili_run.py
@@ -12,4 +12,4 @@ if  os.path.exists("/ql/scripts/bilibili_config.json") == False:
     os.system("cp /ql/repo/zijiahong_bilibili/config.json  /ql/scripts/bilibili_config.json")
     print("bilibili_config.json 配置文件不存在,已经为您在脚本中创建,请修改固定参数重新运行")
     exit()
-os.system("java -jar /ql/repo/zijiahong_bilibili/BILIBILI-HELPER.jar /ql/repo/zijiahong_bilibili/bilibili_config.json")
+os.system("java -jar /ql/repo/zijiahong_bilibili/BILIBILI-HELPER.jar /ql/scripts/bilibili_config.json")
